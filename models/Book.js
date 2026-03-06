cc32f87c6f219b469a7ea987a65fcc3212456ed3
noagortler <noagortler@gmail.com>
2026-03-05 21:01:36 -0800

    Book schemas created

---
+++ models/Book.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+
+const historySchema = new mongoose.Schema ({
+    userName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    checkoutDate: {
+        type: Date,
+        required: true
+    },
+
+    returnDate :{
+        type: Date,
+        required: true
+    },
+
+}, {_id: false});
+
+const availabilitySchema = new mongoose.Schema({
+    total: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 1
+    },
+
+    available: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 1
+    },
+}, {_id: false});
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    isbn: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    genre: {
+        type: String,
+        trim: true
+    },
+
+    year: {
+        type: Number,
+        min: 0
+    },
+
+    description: {
+        type: String,
+        trim: true
+    },
+
+    age: {
+        type: Number,
+        min: 0
+    },
+
+    availability: {
+        type: availabilitySchema,
+        default: () => ({
+            total: 1,
+            available: 1
+        })
+    },  
+    
+    history: {
+        type: [historySchema],
+        default: []
+    },
+},
+{timeStamps: true});

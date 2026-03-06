747a53526732ed8c8f22a4e56d7702fd3bedd9c1
Sam J <sam_joseph@live.com>
2026-03-05 19:29:10 -0800

    Created author routes

---
+++ controllers/authorController.js
@@ -0,0 +1,29 @@
+exports.getAllAuthors = (req, res) => {
+    res.json({
+        message: "Retreiving all the authors"
+    });
+};
+
+exports.getAuthorById = (req, res) => {
+    res.status(200).json({
+        message: `Author id ${req.params.id} retreived`
+    });
+};
+
+exports.createNewAuthor = (req, res) => {
+    res.status(201).json({
+        message: "Author created"
+    });
+};
+
+exports.updateAuthor = (req, res) => {
+    res.json({
+        message: ` Author id ${req.params.id} updated`
+    });
+};
+
+exports.deleteAuthor = (req, res) => {
+    res.status(200).json({
+        message: `Author id ${req.params.id} deleted`
+    });
+};
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json())
 const bookRoutes = require("./routes/bookRoutes");
+const authorRoutes = require("./routes/authorRoutes");
 
 const PORT = 3000;
 
 app.use("/books", bookRoutes);
+app.use("/authors", authorRoutes);
+
 app.get("/", (req, res) => {
     res.json({greet: "Hello World"});
 });

7b3558857b328f3ac41cda7a823fdd855b08d74b
Sam J <sam_joseph@live.com>
2026-03-10 21:07:29 -0700

    feat: add create book route

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -7,6 +7,9 @@ function formatDate(dateValue) {
     return new Date(dateValue).toISOString().split("T")[0];
 };
 
+function buildAuthorName(dbAuthor){
+    return `${dbAuthor.firstName} ${dbAuthor.lastName}`;
+};
 
 
 exports.getAllbooks = async (req, res) => {
@@ -30,6 +33,75 @@ exports.getAllbooks = async (req, res) => {
 };
 
 exports.createBook = async (req, res) => {
+    try{
+        const { 
+            title,
+            isbn,
+            authorId,
+            genre,
+            year,
+            availability,
+            description,
+            age
+        } = req.body;
+        if(!title || !isbn || !authorId){
+            res.status(400).json({
+                message: "title, isbn and authorId details are required"
+            });
+        };
+
+        if(!mongoose.Types.ObjectId.isValid(authorId)){
+            res.status(400).json({
+                message: "authorId not found"
+            });
+        };
+
+        const author = await Author.findbyId(authorId);
+        if (!author){
+            res.status(404).json({
+                message: "Author not found"
+            });
+        };
+
+        const authorName = buildAuthorName(author);
+        const totalCopies = availability?.total ?? 1;
+        const newBook = new Book({
+            title,
+            isbn,
+            authorId,
+            author: authorName,
+            genre,
+            year,
+            description,
+            age,
+            availability: {
+                total: totalCopies,
+                available: totalCopies
+            }
+        });
+
+        const savedBook = await newBook.save();
+        const response = {
+            id: savedBook._id,
+            title: savedBook.title,
+            isbn: savedBook.isbn,
+            author: savedBook.author,
+            genre: savedBook.genre,
+            year: savedBook.year,
+            description: savedBook.description,
+            age: savedBook.age,
+            availability: savedBook.availability,
+            history: []
+        };
+
+        res.status(201).json(response);
+
+    }catch(err){
+        res.status(400).json({
+            message: "Error creating book"
+        });
+    };
+   
     res.status(201).json({
         message: "You've created a book"
     });
